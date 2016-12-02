280dddf20f3aa63d95858d369cf26b935fcfb46f
Fabio Lopes <fzlopes1@gmail.com>
2016-12-02 03:29:17 -0200

    Estudando rotas

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,57 @@
 | to using a Closure or controller method. Build something great!
 |
 */
+Route::group(['prefix' => 'painel', 'middleware' => 'auth'], function() {
+	Route::get('/users', function () {
+    return 'Controle de Users';
+	});
+
+	Route::get('/financeiro', function () {
+    return 'Financeiro Painel';
+	});
+
+	Route::get('/', function () {
+    return 'DashBoard';
+	});
+
+});
+
+Route::get('/login', function () {
+    return '#form login';
+	});
+
+Route::get('/categoria2/{idCat?}', function($idCat=null) {
+	return "Posts da categoria {$idCat}";
+});
+
+Route::get('/categoria/{idCat}', function($idCat) {
+	return "Posts da categoria {$idCat}";
+});
+
+Route::get("nome/nome2/nome6", function() {
+	return 'Rota grande';
+})->name('rota.nomeada');
+
+Route::any('/any', function () {
+    return 'Route any'; 
+});
+
+Route::match(['get', 'post'], '/match', function() {
+	return 'Route match';
+});
+
+Route::post('/post', function () {
+    return 'Route Post'; 
+});
+
+Route::get('/contato', function () {
+    return 'Contato';
+});
+
+Route::get('/empresa', function () {
+    return view('empresa');
+});
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('rota.nomeada');
 });
