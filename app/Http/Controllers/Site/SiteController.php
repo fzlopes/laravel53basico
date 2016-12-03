278df53c542cf244348e55c9f33c1ba18e4c8056
Fabio Lopes <fzlopes1@gmail.com>
2016-12-03 03:02:31 -0200

    Criacao de views

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -7,9 +7,28 @@ use App\Http\Controllers\Controller;
 
 class SiteController extends Controller
 {
+    public function __construct()
+    {
+        //$this->middleware('auth');
+        //$this->middleware('auth')
+                   // ->only([
+                     //   'contato',
+                    //    'categoria'
+                    //]);
+        //$this->middleware('auth')
+                    //->except([
+                        //'index',
+                        //'contato'
+                    //]);
+
+    }
+
     public function index()
     {
-    	return 'Home page do site';
+        $teste = 123;
+        $teste2 = 321;
+        $teste3 = 132;
+    	return view('site.home.teste', compact('teste', 'teste2', 'teste3'));
     }
 
     public function contato()
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | to using a Closure or controller method. Build something great!
 |
 */
+Route::resource('painel/produtos', 'Painel\ProdutoController');
 Route::group(['namespace' => 'Site'], function() {
 	Route::get('/categoria/{id}','SiteController@categoria');
 	Route::get('/categoria2/{id?}','SiteController@categoriaOp');
